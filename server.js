adbd225eac9ae506714101d7a9b5ff8693af6371
SAWADOGO Rimsomgdo Roger <rimssawadogo@gmail.com>
2024-10-03 21:01:16 +0100

    Premier commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express=require("express");
+const path=require('path');
+const app=express();
+const port=process.env.port||8000;
+
+app.use(express.static(path.join(__dirname,'public')));
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,'public','book.html'));
+});
+
+app.listen(port,()=>{
+    console.log("server is running");
+})
